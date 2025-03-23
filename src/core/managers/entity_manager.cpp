dab57c646634709d96cbe273d895119fdf705eaf
Oylsister <kkuiba2009@gmail.com>
2025-03-24 01:31:40 +0700

    Incredible miss typo

--- src/core/managers/entity_manager.cpp
+++ src/core/managers/entity_manager.cpp
@@ -79,9 +79,10 @@ void EntityManager::OnAllInitialized()
     if (!CBaseEntity_EmitSoundFilter)
     {
         CSSHARP_CORE_CRITICAL("Failed to find signature for \'CBaseEntity_EmitSoundFilter\'");
+    }
+
     CBaseEntity_DispatchSpawn = (decltype(CBaseEntity_DispatchSpawn))(
         (modules::server->FindSignature(globals::gameConfig->GetSignature("CBaseEntity_DispatchSpawn"))));
-    }
 
     if (!CBaseEntity_DispatchSpawn) 
     {

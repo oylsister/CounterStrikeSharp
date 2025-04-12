2a5e236e8cb3b4ca1ffd59828fd66779dbe63440
Oylsister <kkuiba2009@gmail.com>
2025-04-12 09:29:45 +0700

    Experiment for OnEntityInput

--- src/core/managers/entity_manager.cpp
+++ src/core/managers/entity_manager.cpp
@@ -70,6 +70,17 @@ void EntityManager::OnAllInitialized()
         return;
     }
 
+    m_pEntityIdentityAccept = reinterpret_cast<EntityIdentityAccept>(
+        modules::server->FindSignature(globals::gameConfig->GetSignature("CEntityIdentity_AcceptInput")));
+
+    if (m_pEntityIdentityAccept == nullptr)
+    {
+        CSSHARP_CORE_CRITICAL("Failed to find signature for \'CEntityIdentity_AcceptInput\'");
+        return;
+    }
+
+    on_entity_input_callback = globals::callbackManager.CreateCallback("OnEntityInput");
+
     CEntityInstance_AcceptInput = decltype(CEntityInstance_AcceptInput)(
         modules::server->FindSignature(globals::gameConfig->GetSignature("CEntityInstance_AcceptInput")));
 
@@ -94,10 +105,10 @@ void EntityManager::OnAllInitialized()
         CSSHARP_CORE_CRITICAL("Failed to find signature for \'CBaseEntity_EmitSoundFilter\'");
     }
 
-    CBaseEntity_DispatchSpawn = (decltype(CBaseEntity_DispatchSpawn))(
-        (modules::server->FindSignature(globals::gameConfig->GetSignature("CBaseEntity_DispatchSpawn"))));
+    CBaseEntity_DispatchSpawn = (decltype(CBaseEntity_DispatchSpawn))((
+        modules::server->FindSignature(globals::gameConfig->GetSignature("CBaseEntity_DispatchSpawn"))));
 
-    if (!CBaseEntity_DispatchSpawn) 
+    if (!CBaseEntity_DispatchSpawn)
     {
         CSSHARP_CORE_CRITICAL("Failed to find signature for \'CBaseEntity_DispatchSpawn\'");
         return;
@@ -105,6 +116,7 @@ void EntityManager::OnAllInitialized()
 
     auto m_hook = funchook_create();
     funchook_prepare(m_hook, (void**)&m_pFireOutputInternal, (void*)&DetourFireOutputInternal);
+    funchook_prepare(m_hook, (void**)&m_pEntityIdentityAccept, (void*)&DetourEntityIdentityAccept);
     funchook_install(m_hook, 0);
 
     // Listener is added in ServerStartup as entity system is not initialised at this stage.
@@ -319,6 +331,29 @@ void DetourFireOutputInternal(CEntityIOOutput* const pThis,
             pCallbackPair->post->ScriptContext().Push(flDelay);
             pCallbackPair->post->Execute();
         }
+    }
+}
+
+void DetourEntityIdentityAccept(CEntityIdentity* pThis,
+                                CUtlSymbolLarge* pInputName,
+                                CEntityInstance* pActivator,
+                                CEntityInstance* pCaller,
+                                variant_t* value,
+                                int nOutputID)
+{
+    m_pEntityIdentityAccept(pThis, pInputName, pActivator, pCaller, value, nOutputID);
+
+    auto callback = globals::entityManager.on_entity_input_callback;
+
+    if (callback && callback->GetFunctionCount())
+    {
+        callback->ScriptContext().Reset();
+        callback->ScriptContext().Push(pThis);
+        callback->ScriptContext().Push(pInputName->String());
+        callback->ScriptContext().Push(pActivator);
+        callback->ScriptContext().Push(pCaller);
+        callback->ScriptContext().Push(nOutputID);
+        callback->Execute();
     }
 }
 
